afbeaba3458a06d4194694907b565b82f86a6a6b
slsh1o <atterdag@zoho.eu>
2020-02-28 17:09:14 +0200

    Create a blog app

---
+++ blog/admin.py
@@ -0,0 +1,4 @@
+"""Configs for admin of blog app."""
+# from django.contrib import admin
+
+# Register your models here.
---
+++ blog/models.py
@@ -0,0 +1,4 @@
+"""Models of blog app."""
+# from django.db import models
+
+# Create your models here.
