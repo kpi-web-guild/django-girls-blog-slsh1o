77c45c5ce2f410aecd9e939a3eac070cf623fb35
slsh1o <slsh1o-git@protonmail.com>
2020-06-13 06:26:55 +0300

    Set up urls for post_list & post_detail

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+"""The blog URL mapping."""
+from django.urls import path
+
+from .views import post_list, post_detail
+
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('post/<int:post_pk>/', post_detail, name='post_detail'),
+]
