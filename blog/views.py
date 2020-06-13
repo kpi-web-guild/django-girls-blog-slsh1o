6e0ca3d2a078c0b9efaaa9b281c2fb80166df7c2
slsh1o <slsh1o-git@protonmail.com>
2020-06-13 06:29:45 +0300

    Add post_list & post_detail views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,18 @@
 """Views of blog app."""
-# om django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request, *args, **kwargs):
+    """Pure Django REST API posts list view."""
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts_list})
+
+
+def post_detail(request, post_pk, *args, **kwargs):
+    """Pure Django post detail view."""
+    post = get_object_or_404(Post, pk=post_pk)
+    return render(request, 'blog/post_detail.htlm', {'post': post})
