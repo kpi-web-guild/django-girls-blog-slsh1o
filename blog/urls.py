8359e3200178b712cc954245e8b9537c2d00c332
slsh1o <slsh1o-git@protonmail.com>
2020-06-25 21:55:40 +0300

    Add urls for post_list_json, post_detail_json views

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from .views import (
     post_list, post_detail,
+    post_list_json, post_detail_json,
     post_create, post_edit
 )
 
@@ -12,4 +13,6 @@ urlpatterns = [
     path('create-post/', post_create, name='post_create'),
     path('post/<int:post_pk>/', post_detail, name='post_detail'),
     path('post/<int:post_pk>/edit', post_edit, name='post_edit'),
+    path('api/posts/', post_list_json, name='post_list_json'),
+    path('api/posts/<int:post_pk>/', post_detail_json, name='post_detail_json'),
 ]
