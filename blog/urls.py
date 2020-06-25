92a0d4d14112cc24d330960afdc457b77ab6dd66
slsh1o <slsh1o-git@protonmail.com>
2020-06-25 21:54:23 +0300

    Add urls for post_create, post_edit views

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 """The blog URL mapping."""
 from django.urls import path
 
-from .views import post_list, post_detail
+from .views import (
+    post_list, post_detail,
+    post_create, post_edit,
+)
 
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('create-post/', post_create, name='post_create'),
     path('post/<int:post_pk>/', post_detail, name='post_detail'),
+    path('post/<int:post_pk>/edit/', post_edit, name='post_edit'),
 ]
