91488aab400386faaa4389110b15a49f860ee7f0
slsh1o <slsh1o-git@protonmail.com>
2020-06-25 21:49:16 +0300

    Create PostForm for post creation

---
+++ blog/forms.py
@@ -0,0 +1,20 @@
+"""Forms fo blog app."""
+from django import forms
+
+from .models import Post
+
+
+MAX_TITLE_LENGTH = 64
+
+
+class PostForm(forms.ModelForm):
+    """New Post creation form."""
+    class Meta:
+        model = Post
+        fields = ['title', 'text']
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) >= MAX_TITLE_LENGTH:
+            raise forms.ValidationError('Title is too long')
+        return title
