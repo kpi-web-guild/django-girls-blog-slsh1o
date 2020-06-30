b46070b9dba50c4f9ca7983080493362944267c3
slsh1o <slsh1o-git@protonmail.com>
2020-06-30 18:16:40 +0300

    Add docstrings for forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -11,10 +11,13 @@ class PostForm(forms.ModelForm):
     """New Post creation form."""
 
     class Meta:
+        """Meta configs for Post form."""
+
         model = Post
         fields = ['title', 'text']
 
     def clean_title(self):
+        """Control length of title of the post."""
         title = self.cleaned_data.get('title')
         if len(title) >= MAX_TITLE_LENGTH:
             raise forms.ValidationError('Title is too long')
